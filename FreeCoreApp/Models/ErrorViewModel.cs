004ba64b6f75be4ce83f8b4e63b7310f4f4b28d9
thanhlong <thanhlong@THANHLONG98>
2019-03-15 01:02:03 +0700

    Khoi tao

---
+++ FreeCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FreeCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
